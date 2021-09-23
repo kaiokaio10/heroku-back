9cc0d99a5d7dfd69d7500575013effa3c5620a72
kaiokaio10 <kgalvo47@gmail.com>
2021-09-23 13:02:07 -0300

    back-heroku

---
+++ src/test/java/heroku/spring/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package heroku.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
